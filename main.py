14783b4241fda1d87935ec512c38eabbcad1edd1
Илья Мудрицкий <askold@Ilas-MacBook-Air.local>
2022-08-08 21:19:08 +0300

    Created main logic to write code into files

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+from src.code_generators import CodeCombiner
+from src.params_handler import ParamsCombiner
+from src.settings import get_logger
+
+from pprint import pprint
+
+log = get_logger("main.log")
+
+def main():
+
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    try:
+        os.mkdir("log")
+        os.mkdir("cards")
+
+    except Exception as e:
+        log.warning(e)
+    
+    # Prepare for getting full code
+    params_combiner = ParamsCombiner()
+    full_cards_info = params_combiner.prepare_all_cards()
+    code_combiner = CodeCombiner(full_cards_info)
+
+    # Get code for all cards and all systems
+    ready_code = code_combiner.get_code_for_all_cards()
+
+    for code in ready_code:
+        # Write code to 'score_card'.md file
+        with open(f"{path}/cards1/{code[0]}.md", "w") as f:
+            # Header
+            f.write(f"# {code[0]}\n\n")
+            # OMDM Code
+            f.write("## OMDM Code\n")
+            f.write("```js\n")
+            for line in code[1]["omdm"].code:
+                f.write(line)
+            f.write("```\n\n")
+
+            # Blaze code
+            f.write("## Blaze code\n")
+            f.write("```js\n")
+            for line in code[1]["blaze"].code:
+                f.write(line)
+            f.write("```\n\n")
+
+            # Report fields
+            f.write("## Report fields\n")
+            f.write("```js\n")
+            for line in code[1]["report"].code:
+                f.write(line)
+            f.write("```\n\n")
+
+if __name__ == "__main__":
+    main()
